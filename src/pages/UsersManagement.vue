f7dad37db3b7158db6287d2bc65e2a43a4966931
Mahesa | ESER <140386522+mahesaPinuntun@users.noreply.github.com>
2026-09-03 23:20:13 +0700

    Update UsersManagement.vue

--- src/pages/UsersManagement.vue
+++ src/pages/UsersManagement.vue
@@ -4,7 +4,7 @@
     <header class="header-section">
       <div class="header-content">
         <div class="top-nav">
-          <router-link to="/dashboard" class="btn-back">
+          <router-link to="/admin" class="btn-back">
             <svg class="icon-sm" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <line x1="19" y1="12" x2="5" y2="12"/><polyline points="12 19 5 12 12 5"/>
             </svg>
