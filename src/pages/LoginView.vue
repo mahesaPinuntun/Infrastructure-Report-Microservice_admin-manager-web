0145dec40f889e71291142cac54d87c7bad83930
Mahesa | ESER <140386522+mahesaPinuntun@users.noreply.github.com>
2026-09-04 14:30:33 +0700

    Update LoginView.vue

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -7,7 +7,7 @@
           <svg class="icon-brand" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>
           </svg>
-          <span>InfraReport</span>
+          <span>エサの ー Infrastructure Report</span>
         </router-link>
       </div>
 
